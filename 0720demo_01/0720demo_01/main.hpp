b54729ea28a126fee2e3f365af46cce024392c66
own4irport <alashicc@qq.com>
2021-07-20 17:33:21 +0800

    first commit

---
+++ 0720demo_01/0720demo_01/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  0720demo_01
+//
+//  Created by admin on 2021/7/20.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
---
+++ 0720demo_01/0720demo_01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0720demo_01
+//
+//  Created by admin on 2021/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
